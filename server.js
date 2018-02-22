36279b90eb4a2c965fd8ce9756cc58706a114f9b
tanz <john.tansor@gmail.com>
2018-02-21 22:20:21 -0600

    adds logic, core functionality, results modal, & fixes bugs

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path")
-
 var app = express();
-
+var dir = path.join(__dirname, 'app/public');
 var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+app.use(express.static(dir));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
